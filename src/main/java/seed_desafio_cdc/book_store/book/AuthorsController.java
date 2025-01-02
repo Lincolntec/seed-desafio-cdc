f930ac684d0d28b1a529ab9570701e522e72977c
Lincoln <lincoln_tec@hotmail.com>
2025-01-02 13:58:04 -0300

    Inclusão da Javax Validation e criação de classe Controller e Author

--- src/main/java/seed_desafio_cdc/book_store/book/AuthorsController.java
+++ src/main/java/seed_desafio_cdc/book_store/book/AuthorsController.java
@@ -1,4 +1,23 @@
 package seed_desafio_cdc.book_store.book;
 
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
 public class AuthorsController {
+
+    @PostMapping(value = "/authors")
+    public String create(@RequestBody @Valid NewAuthorRequest request) {
+
+        System.out
+                .println(
+                        "Test de URI"
+                );
+
+        return "Creating authors...";
+    }
 }
---
+++ src/main/java/seed_desafio_cdc/book_store/book/NewAuthorRequest.java
@@ -0,0 +1,27 @@
+package seed_desafio_cdc.book_store.book;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class NewAuthorRequest {
+
+    @NotBlank
+    private String name;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @Size(max = 400)
+    private String description;
+
+    public NewAuthorRequest(@NotBlank String name,
+                            @NotBlank @Email String email,
+                            @NotBlank @Size(max = 400) String description) {
+        super();
+        this.name = name;
+        this.email = email;
+        this.description = description;
+    }
+}
